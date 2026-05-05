0f856c4628ecd3733e95c0cfa05a13987822fd5d
Valdemar <54585769+Hollicsh@users.noreply.github.com>
2026-05-06 03:41:55 +0500

    Create ruRU.lua

---
+++ Locales/ruRU.lua
@@ -0,0 +1,88 @@
+--[[
+    MythicPulse - Russian Localization (ruRU)
+]]
+
+local _, MP = ...
+
+MP.L = {
+    -- General
+    ["ADDON_LOADED"]         = "MythicPulse загружен. Напишите в чат /mp help для отображения команд.",
+    ["UNKNOWN"]              = "Неизвестно",
+    ["ENABLED"]              = "Вкл.",
+    ["DISABLED"]             = "Выкл.",
+
+    -- Timer
+    ["TIMER_TITLE"]          = "Таймер",
+    ["TIMER_PLUS_TWO"]       = "+2",
+    ["TIMER_PLUS_THREE"]     = "+3",
+    ["TIMER_OVERTIME"]       = "Время вышло",
+    ["TIMER_COMPLETED"]      = "Завершено!",
+    ["TIMER_DEPLETED"]       = "Неудача",
+    ["BOSS_SPLIT"]           = "Босс %d: %s",
+
+    -- Death Tracker
+    ["DEATHS"] = "Смерти",
+    ["DEATH_LOG"] = "Журнал смертей",
+    ["DEATH_PENALTY"] = "Потеряно времени",
+    ["DEATH_ENTRY"] = "%s умер в %s",
+    ["NO_DEATHS"] = "Нет смертей",
+
+    -- Enemy Forces
+    ["ENEMY_FORCES"]         = "Силы противников",
+    ["FORCES_PROGRESS"]      = "%s / %s (%.1f%%)",
+    ["FORCES_COMPLETE"]      = "Силы противников завершены!",
+    ["CURRENT_PULL"]         = "Текущий пулл",
+
+    -- Affixes
+    ["AFFIXES"]              = "Аффиксы",
+
+    -- Keystone
+    ["KEYSTONE"]             = "Ключ",
+    ["YOUR_KEY"]             = "Ваш ключ",
+    ["NO_KEY"]               = "Нет ключа",
+    ["PARTY_KEYS"]           = "Ключи группы",
+    ["KEY_FORMAT"]           = "%s +%d",
+
+    -- Party Cooldowns
+    ["PARTY_CDS"]            = "Перезарядки группы",
+    ["CD_READY"]             = "Готов",
+
+    -- Dispel Tracker
+    ["DISPELS"]              = "Рассеивания",
+    ["DISPEL_READY"]         = "ГОТОВ",
+
+    -- Combat Res
+    ["BREZ"]                 = "Воскрешение в бою",
+    ["BREZ_READY"]           = "%d/%d готов",
+    ["BREZ_NEXT"]            = "следующий через %s",
+    ["BREZ_NONE"]            = "Нет воскрешений в Вашей группе",
+
+    -- Dungeon History
+    ["HISTORY"]              = "История прохождений",
+    ["HISTORY_EMPTY"]        = "Прохождений пока не зафиксировано.",
+    ["HISTORY_TIMED"]        = "Пройдено вовремя",
+    ["HISTORY_DEPLETED"]     = "Неудача",
+
+    -- Config
+    ["CONFIG_TITLE"]         = "Настройки MythicPulse",
+    ["CONFIG_GENERAL"]       = "Общее",
+    ["CONFIG_MODULES"]       = "Модули",
+    ["CONFIG_SCALE"]         = "Масштаб интерфейса",
+    ["CONFIG_OPACITY"]       = "Прозрачность",
+    ["CONFIG_LOCK"]          = "Блокировка фреймов",
+    ["CONFIG_RESET_POS"]     = "Сбросить положение",
+    ["CONFIG_RESET_ALL"]     = "Сбросить все настройки",
+
+    -- Interrupt Tracker
+    ["INTERRUPTS"]           = "Прерывания",
+
+    -- Dungeon Utility
+    ["UTILITY_TITLE"]        = "Полезные функции подземелья",
+    ["UTILITY_NO_ABILITIES"] = "Нет полезных функций для этого подземелья",
+    ["UTILITY_SELECT"]       = "Выбрать подземелье...",
+    ["UTILITY_KNOWN"]        = "Известно",
+    ["UTILITY_ADD"]          = "Добавить",
+    ["UTILITY_REMOVE"]       = "Удалить",
+    ["UTILITY_OPTIONAL"]     = "Необязательно",
+    ["UTILITY_SELF_ONLY"]    = "Способность, доступная только самому себе",
+}
